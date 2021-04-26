a2f5b255eb3aae52fd348d24fdd0bf48ded4b5f5
Harsh <harsh007fc@gmail.com>
2021-04-26 13:26:40 +0530

    code added upto modal container

--- Activity/app.js
+++ Activity/app.js
@@ -1,5 +1,9 @@
 let colorBtn = document.querySelectorAll(".filter_color");
 let mainContainer = document.querySelector(".main_container");
+let body = document.body;
+
+let plusButton = document.querySelector(".fa-plus");
+
 for(let i = 0; i < colorBtn.length; i++)
 {
     colorBtn[i].addEventListener("click",function(e)
@@ -8,3 +12,43 @@ for(let i = 0; i < colorBtn.length; i++)
         mainContainer.style.backgroundColor  = color;
     })
 }
+
+
+plusButton.addEventListener("click", function () {
+    let modal_container = document.createElement("div");
+    modal_container.setAttribute("class", "modal_container");
+    modal_container.innerHTML = `<div class="input_container">
+    <textarea class="modal_input" placeholder="Enter Your Task"></textarea>
+</div>
+<div class="modal_filter_container">
+    <div class="popup_filter pink"></div>
+    <div class="popup_filter blue"></div>
+    <div class="popup_filter green"></div>
+    <div class="popup_filter black"></div>
+</div>`;
+
+    body.appendChild(modal_container);
+})
+
+
+
+
+
+
+
+
+
+
+
+
+{/* <div class="modal_container">
+    <div class="input_container">
+        <textarea class="modal_input" placeholder="Enter Your Task"></textarea>
+    </div>
+    <div class="modal_filter_container">
+        <div class="popup_filter pink"></div>
+        <div class="popup_filter blue"></div>
+        <div class="popup_filter green"></div>
+        <div class="popup_filter black"></div>
+    </div>
+</div> */}
