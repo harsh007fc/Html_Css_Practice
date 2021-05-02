8105f658caa45c007af2dcad555576b690a4db35
Harsh <harsh007fc@gmail.com>
2021-05-02 12:47:38 +0530

    code added uopto localstorage

--- Raw/poc/to_do_app/app.js
+++ Raw/poc/to_do_app/app.js
@@ -2,6 +2,28 @@ let input  = document.querySelector(".input_box");
 
 let ul = document.querySelector(".task_list")
 
+let arr = [];
+
+
+if(localStorage.getItem("allTask"))
+{
+    let stringArr = localStorage.getItem("allTask");
+    arr = JSON.parse(stringArr);
+
+    for(let i = 0; i < arr.length; i++)
+    {
+        let li = document.createElement("li");
+        li.innerText = arr[i];
+        li.addEventListener("dblclick",function ()
+        {
+            li.remove();
+        })
+        li.setAttribute("class","task");
+        ul.appendChild(li);
+        input.value = " ";
+    }
+}
+
 input.addEventListener("keydown",function(e)
 {
     // console.log("some key was pressed ");
@@ -15,16 +37,25 @@ input.addEventListener("keydown",function(e)
 
         //create any html elment
         let li = document.createElement("li");
-         
+        li.innerText = task;
 
+        if (localStorage.getItem("allTask")) {
+            let stringArr = localStorage.getItem("allTask");
+            arr = JSON.parse(stringArr);
+        }
+
+        arr.push(task);
+        let stringArr = JSON.stringify(arr);
+        localStorage.setItem("allTask",stringArr);
         ///event  added to remove a list item
         li.addEventListener("dblclick",function ()
         {
             li.remove();
+            
         })
 
 
-        li.innerText = task;
+        
         li.setAttribute("class","task");
          //append as a child
         ul.appendChild(li);
